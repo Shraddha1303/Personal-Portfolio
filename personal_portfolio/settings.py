acbcb2108584e6195fefa189299f4b99146c5007
Shraddha <shraddha13.patange@gmail.com>
2022-04-22 12:58:41 +0530

    New local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3-&b43a0p-brz%*_!v34w!bp+p*-uf++eqky+hps9#@ogonmvl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shraddhap13.pythonanywhere.com']
 
 
 # Application definition
@@ -119,10 +119,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+
+try:
+    from . local_settings import *
+
+except ImportError:
+    print("No local file, should be in production")
+
